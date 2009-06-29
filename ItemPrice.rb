98cb3d35d90c8657987fa60168dda2edcbecad8b
Kevin Laborda <klaborda@gmail.com>
2009-06-29 16:16:22 -0600

    First! \o/

---
+++ ItemPrice.rb
@@ -0,0 +1,48 @@
+require 'net/http'
+require 'uri'
+require 'rexml/document'
+require 'rubygems'
+require 'sqlite3'
+include REXML
+
+class ItemPrice
+	def initialize
+		@url_base = "http://api.eve-central.com/api/marketstat?"
+		@regionlimit = "10000002"
+	end
+
+	def namesToIDs(names)
+		ids = Array.new
+		names.each { |name|
+			ids << nameToID(name)
+		}
+		return ids
+	end
+
+	def nameToID(sStr)
+		die("Database not found") if !File.exists?('invTypes.db') 
+		db = SQLite3::Database.new("invTypes.db")
+		sql = "SELECT typeID FROM invTypes WHERE typeName LIKE '#{sStr}%' LIMIT 1"
+		return db.get_first_value(sql)
+	end
+
+	def priceMatch(ids)
+		typeIDs = ""
+		ids.each { |id|
+			typeIDs << "typeid=#{id}&"
+		}
+		typeIDs.chop!
+		url = @url_base + typeIDs + "&regionlimit=#{@regionlimit}"
+		
+		src = Net::HTTP::get URI::parse(url)
+		xmldoc = Document.new(src)
+		XPath.each(xmldoc,'//sell/median') { |e|
+			puts "#{e.text}"
+		}
+	end
+
+	def die(msg)
+		puts "\n#{msg}\n"
+		exit
+	end
+end
